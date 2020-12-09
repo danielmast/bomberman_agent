7f45345ce40d8f3207a146adff325f3b564c886e
danielmast <danielmast92@gmail.com>
2020-12-09 16:38:42 +0100

    First check if screen = gameplay (more efficient)

--- files/serpent_Bomberman_game_agent.py
+++ files/serpent_Bomberman_game_agent.py
@@ -20,6 +20,24 @@ class SerpentBombermanGameAgent(GameAgent):
     def handle_play(self, game_frame):
         current_screen = self.get_current_screen(game_frame)
         print ('current_screen = ', current_screen)
+
+        if current_screen != 'GAMEPLAY':
+            self.handle_menu(current_screen)
+            return
+
+        action = random.randint(0, 5)
+        if action == 0:
+            self.input_controller.tap_key(KeyboardKey.KEY_LEFT)
+        elif action == 1:
+            self.input_controller.tap_key(KeyboardKey.KEY_RIGHT)
+        elif action == 2:
+            self.input_controller.tap_key(KeyboardKey.KEY_UP)
+        elif action == 3:
+            self.input_controller.tap_key(KeyboardKey.KEY_DOWN)
+        elif action == 4:
+            self.input_controller.tap_key(KeyboardKey.KEY_SPACE)
+
+    def handle_menu(self, current_screen):
         if current_screen == 'SPLASH':
             self.game.api.Menu.click_play_button()
             sleep(0.5)
@@ -34,22 +52,12 @@ class SerpentBombermanGameAgent(GameAgent):
         elif current_screen == 'END OF GAME':
             self.game.api.Menu.click_play_again_button()
             sleep(0.5)
-        else:
-            action = random.randint(0, 5)
-            if action == 0:
-                self.input_controller.tap_key(KeyboardKey.KEY_LEFT)
-            elif action == 1:
-                self.input_controller.tap_key(KeyboardKey.KEY_RIGHT)
-            elif action == 2:
-                self.input_controller.tap_key(KeyboardKey.KEY_UP)
-            elif action == 3:
-                self.input_controller.tap_key(KeyboardKey.KEY_DOWN)
-            elif action == 4:
-                self.input_controller.tap_key(KeyboardKey.KEY_SPACE)
 
 
     def get_current_screen(self, game_frame):
-        if self.is_located(self.game.sprites['SPRITE_SPLASH_SCREEN_AVATAR_BELLY'], game_frame):
+        if self.is_located(self.game.sprites['SPRITE_GAMEPLAY_WALL'], game_frame):
+            return "GAMEPLAY"
+        elif self.is_located(self.game.sprites['SPRITE_SPLASH_SCREEN_AVATAR_BELLY'], game_frame):
             return "SPLASH"
         elif self.is_located(self.game.sprites['SPRITE_PLAYER_MENU_1_PLAYER_BUTTON'], game_frame):
             return "PLAYER_MENU"
