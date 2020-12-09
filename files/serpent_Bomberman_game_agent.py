d5b841397c49b9b68a14f6f9d24e995412a76ea7
danielmast <danielmast92@gmail.com>
2020-12-09 14:24:14 +0100

    Use API to click through menu + cleanup

--- files/serpent_Bomberman_game_agent.py
+++ files/serpent_Bomberman_game_agent.py
@@ -16,23 +16,11 @@ class SerpentBombermanGameAgent(GameAgent):
         pass
 
     def handle_play(self, game_frame):
-        print("Start agent")
-
-        # if self.frame_counter % 2 == 0:
-        #     self.input_controller.tap_key(KeyboardKey.KEY_RIGHT)
-        # else :
-        #     self.input_controller.tap_key(KeyboardKey.KEY_LEFT)
-        # self.frame_counter += 1
-
         self.click_through_menu(game_frame)
 
 
     def click_through_menu(self, game_frame):
-        print('Locating SCREEN1_PLAY_BUTTON')
-        sprite_locator = SpriteLocator()
-        sprite = self.game.sprites['SPRITE_SCREEN1_PLAY_BUTTON']
-        print(sprite)
-        location = sprite_locator.locate(sprite=sprite, game_frame=game_frame)
-        print(location)
-
-        self.input_controller.click_screen_region(screen_region='SCREEN1_PLAY_BUTTON')
+        self.game.api.Menu.click_play_button()
+        self.game.api.Menu.click_1_player_button()
+        self.game.api.Menu.click_1_opponent_button()
+        self.game.api.Menu.click_level_1_button()
